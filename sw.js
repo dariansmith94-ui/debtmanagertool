083dd9667d87f8a665c867e02ab15c91bbfa359e
dariansmith94-ui <dariansmith94@me.com>
2026-04-20 00:26:03 -0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = “debtfree-v1”;
+const ASSETS = [”/”, “/index.html”, “/manifest.webmanifest”];
+
+self.addEventListener(“install”, function(e) {
+e.waitUntil(
+caches.open(CACHE).then(function(cache) {
+return cache.addAll(ASSETS);
+})
+);
+self.skipWaiting();
+});
+
+self.addEventListener(“activate”, function(e) {
+e.waitUntil(
+caches.keys().then(function(keys) {
+return Promise.all(
+keys.filter(function(k){ return k !== CACHE; })
+.map(function(k){ return caches.delete(k); })
+);
+})
+);
+self.clients.claim();
+});
+
+self.addEventListener(“fetch”, function(e) {
+e.respondWith(
+caches.match(e.request).then(function(cached) {
+return cached || fetch(e.request).then(function(response) {
+// Cache new requests as they come in
+var clone = response.clone();
+caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
+return response;
+});
+}).catch(function() {
+// Offline fallback
+return caches.match(”/index.html”);
+})
+);
+});
